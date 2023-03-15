6f79c57a96404ca9e59bce65abd6a8957b983fe4
martin mithi <gathitumithi9000@gmail.com>
2023-03-15 23:25:51 +0300

    Add movie view route

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, url_for
 
 @app.route('/')
 def index():
@@ -9,6 +9,13 @@ def index():
 @app.route('/movies')
 def movies():
     return render_template("movies.html")
+
+@app.route('/movie/<int:movie_id>')
+def movie(movie_id):
+    '''
+    View movie page function that returns the movie details page and it's data
+    '''
+    pass
 
 @app.route('/series')
 def series():
