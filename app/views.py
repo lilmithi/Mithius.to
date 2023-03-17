6b808ada9c3ec9e59a0ad8bd709267f8fae50ca1
martin mithi <gathitumithi9000@gmail.com>
2023-03-17 23:01:20 +0300

    Add view functions for Browse trends

--- app/views.py
+++ app/views.py
@@ -4,7 +4,7 @@ from flask import render_template, redirect, url_for
 @app.route('/')
 def index():
     title = "Hello World!"
-    return render_template("index.html",title = title)
+    return render_template("index.html",title = title, type = "movies", is_browse = True)
 
 # Movies
 
@@ -24,35 +24,35 @@ def popular_movies():
     '''
     View movie function that returns popular movies
     '''
-    return render_template("popular.html", type = "movies")
+    return render_template("categories.html", type = "movies", category = "popular")
 
 @app.route('/movies-az')
 def az_movies():
     '''
     View movie function that returns A-Z page
     '''
-    return render_template("az.html", type = "movies")
+    return render_template("categories.html", type = "movies", category = "az")
 
 @app.route('/movies-genre')
 def genre_movies():
     '''
     View movie function that returns movies based on genre
     '''
-    return render_template("genre.html", type = "movies")
+    return render_template("categories.html", type = "movies", category = "genre")
 
 @app.route('/movies-language')
 def movies_language():
     '''
     View movie function that returns movies based on language
     '''
-    return render_template("language.html", type = "movies")
+    return render_template("categories.html", type = "movies", category = "language")
 
 @app.route('/movies-year')
 def movies_year():
     '''
     View movie function that returns movies based on year
     '''
-    return render_template("year.html", type = "movies")
+    return render_template("categories.html", type = "movies", category = "year")
 
 # Series
 
@@ -73,32 +73,68 @@ def popular_series():
     '''
     View series function that returns popular series
     '''
-    return render_template("popular.html", type = "series")
+    return render_template("categories.html", type = "series", category = "popular")
 
 @app.route('/series-az')
 def az_series():
     '''
     View series function that returns A-Z page
     '''
-    return render_template("az.html", type = "series")
+    return render_template("categories.html", type = "series", category = "az")
 
 @app.route('/series-genre')
 def genre_series():
     '''
     View series function that returns series based on genre
     '''
-    return render_template("genre.html", type = "series")
+    return render_template("categories.html", type = "series", category = "genre")
 
 @app.route('/series-language')
 def series_language():
     '''
     View series function that returns series based on language
     '''
-    return render_template("language.html", type = "series")
+    return render_template("categories.html", type = "series", category = "language")
 
 @app.route('/series-year')
 def series_year():
     '''
     View series function that returns series based on year
     '''
-    return render_template("year.html", type = "series")
+    return render_template("categories.html", type = "series", category = "year")
+
+@app.route('/trends-az')
+def trends_az():
+    '''
+    View trends function that returns shows based on alphabetical order
+    '''
+    return render_template("trends.html", type = "az")
+
+@app.route('/trends-genre')
+def trends_genre():
+    '''
+    View trends function that returns shows based on genre
+    '''
+    return render_template("trends.html", type = "genre")
+
+@app.route('/trends-language')
+def trends_language():
+    '''
+    View function that returns shows based on language
+    '''
+    return render_template("trends.html", type = "language")
+
+@app.route('/trends-popular')
+def trends_popular():
+    '''
+    View function that returns shows based on popularity
+    '''
+    return render_template("trends.html", type = "popular")
+
+@app.route('/trends-year')
+def trends_year():
+    '''
+    View function that returns shows based on year
+    '''
+    return render_template("trends.html", type = "year")
+
