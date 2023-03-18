1eda436439f410a6999981831230126684bb760a
martin mithi <gathitumithi9000@gmail.com>
2023-03-18 08:48:28 +0300

    Update parent constructor __init__ method

--- app/models/show.py
+++ app/models/show.py
@@ -2,4 +2,18 @@ class Show:
     '''
     Show parent class
     '''
-    pass
+    reviews = []
+
+    def __init__(self, id, genre, title, language, imageurl, media_type, release_date, popularity, overview, rating) -> None:
+        self.id = id
+        self.genre = genre
+        self.title = title
+        self.language = language
+        self.imageurl = imageurl
+        self.media_type = media_type
+        self.release_date = release_date
+        self.popularity = popularity
+        self.overview = overview
+        self.rating = rating
+
+    
